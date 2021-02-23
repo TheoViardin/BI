98ce6000fac1e71622c59516493a8057045b2bd7
TheoViardin <theoviardin@gmail.com>
2021-02-23 16:32:43 +0100

    first commit

---
+++ poms/jobs/process/remplir_datawarehouse_0.1/src/main/templates/Job_run_template.ps1
@@ -0,0 +1,3 @@
+$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+cd $fileDir
+java '-Xms256M' '-Xmx1024M' '-Dfile.encoding=UTF-8' -cp '.;../lib/routines.jar;../lib/advancedPersistentLookupLib-1.2.jar;../lib/commons-collections-3.2.2.jar;../lib/dom4j-1.6.1.jar;../lib/jboss-serialization.jar;../lib/log4j-1.2.15.jar;../lib/log4j-1.2.16.jar;../lib/mysql-connector-java-5.1.30-bin.jar;../lib/trove.jar;remplir_datawarehouse_0_1.jar;ventes_0_1.jar;achats_0_1.jar;' so_emballage.remplir_datawarehouse_0_1.Remplir_datawarehouse  %* 
---
+++ poms/jobs/process/remplir_datawarehouse_0.1/src/main/templates/Job_run_template.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd `dirname $0`
+ROOT_PATH=`pwd`
+java -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .:$ROOT_PATH:$ROOT_PATH/../lib/routines.jar:$ROOT_PATH/../lib/advancedPersistentLookupLib-1.2.jar:$ROOT_PATH/../lib/commons-collections-3.2.2.jar:$ROOT_PATH/../lib/dom4j-1.6.1.jar:$ROOT_PATH/../lib/jboss-serialization.jar:$ROOT_PATH/../lib/log4j-1.2.15.jar:$ROOT_PATH/../lib/log4j-1.2.16.jar:$ROOT_PATH/../lib/mysql-connector-java-5.1.30-bin.jar:$ROOT_PATH/../lib/trove.jar:$ROOT_PATH/remplir_datawarehouse_0_1.jar:$ROOT_PATH/ventes_0_1.jar:$ROOT_PATH/achats_0_1.jar: so_emballage.remplir_datawarehouse_0_1.Remplir_datawarehouse  "$@" 
